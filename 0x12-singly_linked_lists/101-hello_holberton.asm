f7c5cfc91b6bbeeff4360ae636742003cb89e5b7
DavidUche75 <daviduche75@gmail.com>
2023-09-24 10:25:50 -0700

    update

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,14 +1,12 @@
-section .data
-    hello_msg db "Hello, Holberton",10,0   ; Message to be printed with newline character
-
-section .text
     global main
     extern printf
-
 main:
     push rbp
     mov rdi, hello_msg
     call printf
     pop rbp
     ret
+format:
+    hello_msg db "Hello, Holberton",10,0   ; Message to be printed with newline character
+
 
